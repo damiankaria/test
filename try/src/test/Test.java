bcb5449fce2b0e9894171e050df2e662f9d47e7d
Damian <d.k.karia@student.utwente.nl>
2015-11-25 11:51:46 +0100

    verandert in a

--- try/src/test/Test.java
+++ try/src/test/Test.java
@@ -2,19 +2,19 @@ package test;
 
 public class Test {
 
-		public static int som(int x){
-			if (x==0) {
+		public static int som(int a){
+			if (a==0) {
 				return 0;
 			}
 			else {
-				return x + som(x-1);
+				return a + som(a-1);
 			}
 		}
 		
 		public static void main(String args[]){
 			int i = 0;
-			for (int x = 0; x <= 153; x++){
-				i = i + x;
+			for (int a = 0; a <= 153; a++){
+				i = i + a;
 			}
 			System.out.println(i);
 		}
